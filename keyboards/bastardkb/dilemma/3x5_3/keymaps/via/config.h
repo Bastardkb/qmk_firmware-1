9db44666a3a06443b5893e6d692d13155491f9ca
casuanoob <gms.rulz@hotmail.com>
2023-04-04 22:49:56 +1000

    disable Auto mouse layer for now

--- keyboards/bastardkb/dilemma/3x5_3/keymaps/via/config.h
+++ keyboards/bastardkb/dilemma/3x5_3/keymaps/via/config.h
@@ -37,6 +37,6 @@
 
 #ifdef POINTING_DEVICE_ENABLE
 // Automatically enable the pointer layer when moving the trackpad.
-#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 3
+//#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
+//#define AUTO_MOUSE_DEFAULT_LAYER 3
 #endif // POINTING_DEVICE_ENABLE
