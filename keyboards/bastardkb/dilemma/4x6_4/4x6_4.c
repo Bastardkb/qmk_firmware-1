f7dc909207147da002c326405236216ca9ba0260
burkfers <finrod09@users.noreply.github.com>
2023-12-26 10:52:23 +0100

    Formatting

--- keyboards/bastardkb/dilemma/4x6_4/4x6_4.c
+++ keyboards/bastardkb/dilemma/4x6_4/4x6_4.c
@@ -106,7 +106,7 @@ bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
         HSV hsv = _get_hsv_for_layer_index(layer);
 
         // Set brightness to the configured interval brighter than current brightness, clamped to 255 (ie. uint8_t max value). This compensates for the dimmer appearance of the underglow LEDs.
-        hsv.v = MIN(rgb_matrix_get_val() + LAYER_INDICATOR_BRIGHTNESS_INC, 255);
+        hsv.v         = MIN(rgb_matrix_get_val() + LAYER_INDICATOR_BRIGHTNESS_INC, 255);
         const RGB rgb = hsv_to_rgb(hsv);
 
         for (int i = led_min; i < led_max; i++) {
